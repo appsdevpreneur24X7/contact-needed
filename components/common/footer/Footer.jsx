2ba9155dc3969030346ae84e1e2e439a126ca086
appsdevpreneur24X7 <appsdevpreneur247@gmail.com>
2024-03-08 04:50:27 +0530

    UI STYLES

--- components/common/footer/Footer.jsx
+++ components/common/footer/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
       <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 pr0 pl0">
         <div className="footer_about_widget">
           <h4>About Site</h4>
-          <p>
+          <p style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }}>
           The biggest part of our digital transformation is changing the way we think.
           </p>
-          <p>
+          <p style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }}>
             &copy; {new Date().getFullYear()} by{" "}
             <a
               href=""
@@ -31,10 +31,10 @@ const Footer = () => {
           <h4>Quick Links</h4>
           <ul className="list-unstyled">
             <li>
-              <Link href="/">About Us</Link>
+              <Link href="/" style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }} >About Us</Link>
             </li>
             <li>
-              <Link href="/">Terms & Conditions</Link>
+              <Link href="/" style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }} >Terms & Conditions</Link>
             </li>
           </ul>
         </div>
@@ -46,21 +46,22 @@ const Footer = () => {
           <h4>Contact Us</h4>
           <ul className="list-unstyled">
             <li>
-              <a href="mailto:hello@contactneeded.com">hello@contactneeded.com</a>
+              <a href="mailto:hello@contactneeded.com" style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }}> hello@contactneeded.com</a>
             </li>
             <li>
-              <a href="#">Opp ISB School</a>
+              <a href="#" style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }} >Opp ISB School</a>
             </li>
             <li>
-              <a href="#">Gachibowli, Hyderabad.</a>
+              <a href="#" style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }}>Gachibowli, Hyderabad.</a>
             </li>
             <li>
-              <a href="tel:+91.7032405627">+91.7032405627</a>
+              <a href="tel:+91.7032405627" style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }}>+91.7032405627</a>
             </li>
           </ul>
         </div>
       </div>
       {/* End .col */}
+      {/* fontWeight: '600', color: '#fff', fontSize: '15px' */}
 
       <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
         <div className="footer_social_widget">
--- components/common/footer/Social.jsx
+++ components/common/footer/Social.jsx
@@ -3,15 +3,13 @@ const Social = () => {
     { id: 1, liveLink: "https://www.facebook.com/", icon: "fa-facebook" },
     { id: 2, liveLink: "https://www.twitter.com/", icon: "fa-twitter" },
     { id: 3, liveLink: "https://www.instagram.com/", icon: "fa-instagram" },
-    { id: 4, liveLink: "https://www.pinterest.com/", icon: "fa-pinterest" },
-    { id: 5, liveLink: "https://www.dribbble.com/", icon: "fa-dribbble" },
   ];
   return (
     <>
       {socialContent.map((item) => (
-        <li className="list-inline-item" key={item.id}>
+        <li className="list-inline-item" key={item.id} style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }}>
           <a href={item.liveLink} target="_blank" rel="noopener noreferrer">
-            <i className={`fa ${item.icon}`}></i>
+            <i className={`fa ${item.icon}`} style={{ fontWeight: '600', color: '#fff', fontSize: '15px' }}></i>
           </a>
         </li>
       ))}
